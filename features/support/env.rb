7f8bd4a1dd92eec8f2c11bbeac6d7e14bbcc3a05
srawlins <sam.rawlins@gmail.com>
2011-10-11 23:19:50 -0700

    Added support for shared-password users, environments other than dev, and screenshotting

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,13 +2,28 @@ require File.join(File.dirname(__FILE__), '..', '..', 'lib', 'kaikifs')
 require 'rspec'
 require 'highline/import'
 require 'active_support/inflector'
+require 'yaml'
 
 class KaikiFSWorld
-    username = ask("NetID:  ")    { |q| q.echo = true }
-    password = ask("Password:  ") { |q| q.echo = "*" }
-    @@kaikifs = KaikiFS::Driver.new(username, password, :envs => ['dev'])
+    SHARED_PASSWORDS_FILE = "shared_passwords.yaml"
+    username   = ENV['NETID']
+    password   = ENV['PASSWORD']
+    if File.exist? SHARED_PASSWORDS_FILE
+      shared_passwords = File.open(SHARED_PASSWORDS_FILE) { |h| YAML::load_file(h) }
+      puts shared_passwords
+      if password.nil? and shared_passwords.keys.any? { |user| username[user] }
+        user_group = shared_passwords.keys.select { |user| username[user] }[0]
+        password = shared_passwords[user_group]
+      end
+    end
+    username ||= ask("NetID:  ")    { |q| q.echo = true }
+    password ||= ask("Password:  ") { |q| q.echo = "*" }
+    env = 'dev'
+    @@kaikifs = KaikiFS::Driver.new(username, password, :envs => [env])
+    @@kaikifs.mk_screenshot_dir(File.join(Dir::pwd, 'features', 'screenshots'))
     @@kaikifs.start_session
-    @@kaikifs.page.open "kfs-dev/portal.jsp"
+    @@kaikifs.page.open "kfs-#{env}/portal.jsp"
+    @@kaikifs.window_maximize
     @@kaikifs.login_via_webauth
 
     at_exit { @@kaikifs.stop }
@@ -18,4 +33,13 @@ end
 
 World do
   KaikiFSWorld.new
+end
+
+After do |scenario|
+  #puts scenario.instance_variables.sort
+  #puts scenario.methods.sort
+  #puts scenario.file_colon_line
+  if scenario.failed?
+    kaikifs.screenshot(scenario.file_colon_line.file_safe + '_' + Time.now.strftime("%Y%m%d%H%M%S"))
+  end
 end
