f319511b17be60b88b54235c760448d34a837da3
Sam Rawlins <sam.rawlins@gmail.com>
2012-08-24 13:26:03 -0700

    Removing debug lines

--- features/support/headless.rb
+++ features/support/headless.rb
@@ -15,16 +15,13 @@ if ! ENV['BUILD_NUMBER'].nil?
 
   After do |scenario|
     if scenario.failed?
-      puts "Failed i guess"
       headless.video.stop_and_save(video_path(scenario))
     else
-      puts "Succeeded maybe"
       headless.video.stop_and_discard
     end
   end
 
   def video_path(scenario)
-    puts "#{scenario.name.split.join("_")}.mov"
     "#{scenario.name.split.join("_")}.mov"
   end
 end
